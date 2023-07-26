dd0a47dfc13fd8b324cf41eb0c8b56f64e5c2079
Dimitris Gerasimou <dimgerasimou@gmail.com>
2023-07-27 02:05:55 +0300

    font2 patch

--- config.h
+++ config.h
@@ -6,6 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *sparefonts[] = {
+	"FiraCode Nerd Font:pixelsize=12:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true",
+};
 static int borderpx = 2;
 
 /*
